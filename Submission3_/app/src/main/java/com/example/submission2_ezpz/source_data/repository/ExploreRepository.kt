bb255a16333e4201bdeff046238fdfc88ca87cca
xwyzworm_ubuntu <pratamaazmi1@gmail.com>
2022-03-17 20:28:52 +0700

    init all repos

---
+++ Submission3_/app/src/main/java/com/example/submission2_ezpz/source_data/repository/ExploreRepository.kt
@@ -0,0 +1,4 @@
+package com.example.submission2_ezpz.source_data.repository
+
+class ExploreRepository{
+}
--- Submission3_/app/src/main/java/com/example/submission2_ezpz/source_data/repository/UserRepository.kt
+++ Submission3_/app/src/main/java/com/example/submission2_ezpz/source_data/repository/UserRepository.kt
@@ -1,19 +1,35 @@
 package com.example.submission2_ezpz.source_data
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.map
+import com.example.submission2_ezpz.source_data.local.entity.UserEntity
 import com.example.submission2_ezpz.source_data.local.networks.ApiInterface
 import com.example.submission2_ezpz.source_data.local.room.UserDao
 import com.example.submission2_ezpz.source_data.local.setting_preference.SettingPreferences
 
 class UserRepository private constructor (
-    private val apiService: ApiInterface,
     private val userDao: UserDao,
     private val userPreferences: SettingPreferences) {
 
 
+    fun getCurrentTheme() : LiveData<Boolean>{
+        return userPreferences.getThemeSettings().asLiveData()
+    }
 
+    fun getFavorites() : LiveData<Result<List<UserEntity>>> {
+        return userDao.getFavorites().map {
+            Result.Success(it)
+        }
+    }
 
+    fun setFavorites(user : UserEntity, favoriteState : Boolean) {
+        user.isFavorite = favoriteState
+        userDao.updateFavorites(user)
+    }
 
     companion object {
+        private val TAG : String = UserRepository::class.simpleName as String
         @Volatile
         private var INSTANCE : UserRepository ? = null
 
