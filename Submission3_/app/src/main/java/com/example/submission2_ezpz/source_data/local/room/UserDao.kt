97251ea1a48d27801e6713d5e8ed53cff79d2998
xwyzworm_ubuntu <pratamaazmi1@gmail.com>
2022-03-17 15:17:35 +0700

    Add Dao and dtabase Implementation

--- Submission3_/app/src/main/java/com/example/submission2_ezpz/source_data/local/room/UserDao.kt
+++ Submission3_/app/src/main/java/com/example/submission2_ezpz/source_data/local/room/UserDao.kt
@@ -1,4 +1,8 @@
 package com.example.submission2_ezpz.source_data.local.room
 
+import androidx.room.Dao
+
+
+@Dao
 interface UserDao {
 }
--- Submission3_/app/src/main/java/com/example/submission2_ezpz/source_data/local/room/UserDatabase.kt
+++ Submission3_/app/src/main/java/com/example/submission2_ezpz/source_data/local/room/UserDatabase.kt
@@ -1,4 +1,29 @@
 package com.example.submission2_ezpz.source_data.local.room
 
-class UserDatabase {
+import android.content.Context
+import android.service.autofill.UserData
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.submission2_ezpz.source_data.local.entity.UserEntity
+
+
+@Database(entities=[UserEntity::class], version=1)
+abstract class UserDatabase () : RoomDatabase() {
+
+    abstract fun userDao() : UserDao
+    
+    companion object {
+        @Volatile
+        private var INSTANCE : UserDatabase? =null
+
+        @JvmStatic
+        fun getDatabaseInstance(context : Context) : UserDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(context, UserDatabase::class.java, "user.db").build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
 }
