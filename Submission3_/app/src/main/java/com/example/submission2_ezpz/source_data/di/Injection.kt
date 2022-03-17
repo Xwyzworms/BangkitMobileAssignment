9df2502c33040c5d3fb75d4789757e5a60c4003b
xwyzworm_ubuntu <pratamaazmi1@gmail.com>
2022-03-17 08:39:12 +0700

    Create Injection Implementatio (ProvideRepository ANd UserSettings)

--- Submission3_/app/src/main/java/com/example/submission2_ezpz/source_data/di/Injection.kt
+++ Submission3_/app/src/main/java/com/example/submission2_ezpz/source_data/di/Injection.kt
@@ -1,4 +1,23 @@
 package com.example.submission2_ezpz.source_data.di
 
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import com.example.submission2_ezpz.source_data.UserRepository
+import com.example.submission2_ezpz.source_data.local.networks.ApiConfig
+import com.example.submission2_ezpz.source_data.local.networks.ApiInterface
+import com.example.submission2_ezpz.source_data.local.room.UserDatabase
+import com.example.submission2_ezpz.source_data.local.setting_preference.SettingPreferences
+
 object Injection {
+    fun ProvideRepository(context : Context,  dataStore : DataStore<Preferences>) : UserRepository{
+        val apiService : ApiInterface = ApiConfig.ApiService()
+        val database  = UserDatabase.getInstance(context)
+        val dao = database.userDao
+        return UserRepository.getInstance(context, ProvideUserSettings(dataStore))
+    }
+
+    fun ProvideUserSettings(datastore : DataStore<Preferences>) : SettingPreferences{
+        return SettingPreferences.getInstance(datastore)
+    }
 }
